1d51d44e253a846ed7251277f5176b2804212109
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-26 15:03:45 -0700

    Added line through MVP Completed

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Item from './Item'
 
 const ItemList = (props) => {
+    
     const {items, handleDelete} = props
     return (
         <div id='component'>
         {items.map((item,index) => { 
-            return <Item item={item} key={index} id={index} handleDelete={handleDelete}/>
+            return <Item  item={item} key={index} id={index} handleDelete={handleDelete}/>
         })}
         </div>
       );
